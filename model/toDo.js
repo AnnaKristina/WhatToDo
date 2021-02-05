b9b38dadb77a4ab57b43069fe553713fdc41a182
AnnaKristina <anna.olsson@medieinstitutet.se>
2021-02-05 12:40:14 +0100

    Created a mongoose Scheme

---
+++ model/toDo.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema ({
+    
+    name: {type:String, require:true},
+    time: {type:String, require:true},
+    date: {type:Date, default:Date.now}
+})
+const Todo = mongoose.model("todo", todoSchema);
+module.exports = Todo; 
