baf7c2fe09d5074269d5cc9fd9ffb4cb856b5463
AnnaKristina <anna.olsson@medieinstitutet.se>
2021-02-06 16:01:38 +0100

    Added ejs files and created a routes map

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Todo = require("/model/toDo.js")
+const bodyParser = require("body-Parser");
+const router = require("./routes/toDoRoute")
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set("view engine", "ejs")
+app.use("/", router)
+
+
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    }
+    
+    mongoose.connect("mongodb+srv://AnnaKristina:4DbrhL8R@cluster0.awfia.mongodb.net/WhatToDo2?retryWrites=true&w=majority", 
+    options, 
+    (err)=>{
+     
+    console.log(err)
+    
+    if (err) return ;
+    
+    console.log("db is connected")
+
+    app.listen(8000, ()=>{
+     console.log("application is running too")
+    })
+    
+    
+    })
---
+++ routes/toDoRoute.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const Todo = require("../model/toDo");
+const router = express.Router();
+
+router.get("/", async (req, res)=>{
+
+    const data = await Todo.find()      
+      
+     res.render("index.ejs", {data:data}) 
+  })
+ 
+ router.post("/", async (req, res) =>{
+   console.log(req.body.name)
+ 
+   await new Todo ({
+     name: req.body.name
+   }).save();
+ 
+   res.redirect("/")
+ })
+ 
+ router.get("/edit/:id", async (req, res) => {
+  const todo = await Todo.findOne({_id:req.params.id})
+  console.log(todo)
+   res.render("edit.ejs", {todo:todo})
+ })
+ 
+ router.post("/edit", async (req, res) => {
+   await Todo.updateOne({_id:req.body.id}, {
+     name: req.body.name
+   })
+ 
+   res.redirect("/")
+ })
+ 
+ 
+ router.get("/delete/:id", async (req, res) => {
+ 
+   await Todo.deleteOne({_id:req.params.id})
+   res.redirect("/")
+ })
+
+ module.exports = router;
