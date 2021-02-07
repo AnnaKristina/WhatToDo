3d86785db0ec1272545fdd7136738174033345f2
AnnaKristina <anna.olsson@medieinstitutet.se>
2021-02-07 14:39:27 +0100

    Added styling, modified mongoose Scheme and refracted some code

--- routes/toDoRoute.js
+++ routes/toDoRoute.js
@@ -3,25 +3,19 @@ const Todo = require("../model/toDo");
 const router = express.Router();
 
 router.get("/", async (req, res)=>{
-
     const data = await Todo.find()      
-      
      res.render("index.ejs", {data:data}) 
   })
  
  router.post("/", async (req, res) =>{
-   console.log(req.body.name)
- 
    await new Todo ({
      name: req.body.name
    }).save();
- 
    res.redirect("/")
  })
  
  router.get("/edit/:id", async (req, res) => {
   const todo = await Todo.findOne({_id:req.params.id})
-  console.log(todo)
    res.render("edit.ejs", {todo:todo})
  })
  
@@ -29,13 +23,10 @@ router.get("/", async (req, res)=>{
    await Todo.updateOne({_id:req.body.id}, {
      name: req.body.name
    })
- 
    res.redirect("/")
  })
  
- 
  router.get("/delete/:id", async (req, res) => {
- 
    await Todo.deleteOne({_id:req.params.id})
    res.redirect("/")
  })
